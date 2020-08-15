7c02076d17a7922ae229210340817067ddad000d
jose castaño <castanojose17@gmail.com>
2020-08-15 13:04:24 -0400

    primer commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.port || 3000;
+
+app.use(express.static( __dirname + '/public'));
+
+
+//express HBS engine
+
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', function (req, res) {
+
+    
+    res.render('home',{
+        nombre: 'jose'
+        
+    });
+})
+
+app.get('/about', function (req, res) {
+
+    res.render('about');
+})
+
+app.get('/data', function (req, res) {
+
+    res.send('Hello data');
+    
+})
+app.listen(port,()=>{
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
